658c817e780290c0124954e88f92fcc5df094b07
Yogesh Singh <yogeshsinghs607@gmail.com>
2018-01-03 23:45:11 +0530

    Initial commit

---
+++ join-a-group.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Join A Group | JECRC Activity Management</title>
+
+	<!--External stylesheet link-->
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+
+	<!--Bootstrap css, js and jq link-->
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+  	<script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+  	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<!--Navigation bar-->
+<nav class="navbar navbar-default navbar-fixed-top navigation">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" style="padding: 6px 0px 0px 50px;" href="index.html"><img src="image/img1_n.png" class="logo"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+      	<li><a href="" class="navi">Join a group</a></li>
+      	<li><a href="" class="navi">Event Registration</a></li>
+        <li><a href="" class="navi">Ask</a></li>
+        <li><a href="" class="navi">Help</a></li>
+        <li><a href="" class="navi">About Us</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+</body>
+</html>
---
+++ jquery.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("#owl-demo").owlCarousel({
+		autoPlay : 3000,
+		item : 4,
+		itemDesktop : [1199,3],
+		itemsDesktopSmall : [979,3]
+	});
+});
